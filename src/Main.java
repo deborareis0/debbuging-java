392f50f61cfb759a2922cbaafbb34343ad030ced
Debs <deborareis0@hotmail.com>
2022-12-28 15:22:15 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package br.com.dio.debbuging;
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Iniciou o programa no método main.");
+            a();
+            System.out.println("Finalizou o programa no método main.");
+        }
+
+        public static void a() {
+            System.out.println("Entrou no método a.");
+            b();
+            System.out.println("Finalizou o método a.");
+        }
+
+        public static void b() {
+            System.out.println("Entrou no método b.");
+            for(int i = 0; i <= 4; i++) System.out.println(i);
+            c();
+            System.out.println("Finalizou o método b.");
+        }
+
+        public static void c(){
+            System.out.println("Entrou no método c.");
+            //Thread.dumpStack(); /*resultado lê de baixo pra cima*/
+            System.out.println("Finalizou o método c.");
+        }
+
     }
-}
